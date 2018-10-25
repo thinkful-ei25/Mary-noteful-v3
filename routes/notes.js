262ab422739f819db72ac632403ee304e3c465ed
Mary Conley <Mary@Marys-MacBook-Pro.local>
2018-10-25 13:00:34 -0700

    updated all the notes routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,50 +1,117 @@
 'use strict';
 
 const express = require('express');
-
+const Note = require('../models/note');
 const router = express.Router();
-
-/* ========== GET/READ ALL ITEMS ========== */
+const mongoose = require('mongoose');
+////* Get All or Complete Search*/
 router.get('/', (req, res, next) => {
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
+  const searchTerm = req.query.searchTerm;
 
+  let titleOrContent = {
+    $or: [
+      { title: { $regex: searchTerm, $options: 'i' } }, //if searchTerm is Null it will look only for title
+      { content: { $regex: searchTerm, $options: 'i' } }//and therefore pull "all"
+    ]
+  }
+  Note.find(titleOrContent)
+    .then(results => {
+      res.json(results)
+    })
+    .catch(err => {
+      next(err);
+    });
 });
-
-/* ========== GET/READ A SINGLE ITEM ========== */
-router.get('/:id', (req, res, next) => {
-
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+////* This finds a particular ID and returns the object associated with*/
+router.get('/:id', (req, res) => {
+  noteId = req.params.id;
+  //creating a 400 error incase id is not valid!
+  if (!mongoose.Types.ObjectId.isValid(noteId)) {
+    const err = new Error('The `id` is NOT valid');
+    err.status = 400;
+    return next(err);
+  }
+  Note.findById(noteId)
+    .then(results => {
+      if (result) {
+        res.json(results);
+      }
+      else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    })
 });
-
-/* ========== POST/CREATE AN ITEM ========== */
-router.post('/', (req, res, next) => {
-
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
-
+////* This is for committing a post/create to the database */
+router.post('/', (req, res) => {
+  const { title, content } = req.body; //destructuring req.body.title & req.body.content
+  /* If there is no title --throw a 400 error */
+  if (!title) {
+    const err = new Error('MISSING `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  newNote = { title, content };
+  Note.create(newNote)
+    .then(result => {
+      res.location(`${req.originalURL}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
-
-/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+////* Updating a single ID! Checking for errors first then running findByIdAndUpdate*/
 router.put('/:id', (req, res, next) => {
+  noteId = req.params.id;
+  const { title, content } = req.body;
+  //creating a 400 error incase id is not valid!
+  if (!mongoose.Types.ObjectId.isValid(noteId)) {
+    const err = new Error('The `id` is NOT valid');
+    err.status = 400;
+    return next(err);
+  }
+  /* If there is no title --throw a 400 error */
+  if (!title) {
+    const err = new Error('MISSING `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  let updateBodyReq = { title, content };
 
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
-
+  Note.findByIdAndUpdate(noteId, updateBodyReq, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      }
+      else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
-
-/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
-router.delete('/:id', (req, res, next) => {
-
-  console.log('Delete a Note');
-  res.status(204).end();
-});
+////* Deleting a single note*/
+router.delete('/:id', req, res, next) => {
+  const noteId = req.params.id;
+//creating a 400 error incase id is not valid!
+  if (!mongoose.Types.ObjectId.isValid(noteId)) {
+    const err = new Error('The `id` is NOT valid');
+    err.status = 400;
+    return next(err);
+  }
+  Note.findByIdAndRemove(noteId)
+  .then(()=> {
+    res.status(204).end();
+  })
+  .catch(err => {
+    next(err);
+  })
+}
 
 module.exports = router;
